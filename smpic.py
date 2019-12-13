91745428bcbf4d5e427054402a3511877673ecd3
skycity233 <544907049@qq.com>
2019-12-13 21:36:10 +0800

    增加了免登录选项

--- smpic.py
+++ smpic.py
@@ -10,13 +10,15 @@
 # =====================================================
 
 
-from os import path,remove
+from os import path, remove
 
-from threading import Timer,Thread
+from threading import Timer, Thread
 from webbrowser import open as openweb
 
-import wx
-import wx.adv
+from wx import Frame, CallAfter, StaticText, NewIdRef, Icon, Panel, TextCtrl, Button, MessageBox, Exit, Menu, App
+from wx import EVT_MENU, STAY_ON_TOP, TE_PASSWORD, EVT_BUTTON
+from wx.adv import TaskBarIcon
+
 import yaml
 
 from PIL import ImageGrab
@@ -27,6 +29,7 @@ from Smms import Smms
 
 current_key_pressed = []
 hotkey = ''
+hotkeys = ''
 keycode = {}
 listener = None
 
@@ -48,8 +51,9 @@ def init_config():
         cont = f.read()
         config = yaml.load(cont, Loader=yaml.BaseLoader)
         try:
-            global hotkey, token, keycode
+            global hotkey, token, keycode, hotkeys
             hotkey = config['hotkey'].lower().split('+')
+            hotkeys = config['hotkey'].lower()
             token = config['token']
 
             for i in range(len(hotkey)):
@@ -59,15 +63,6 @@ def init_config():
 
         except KeyError:
             pass
-
-
-def set_token_0():
-    with open('config.yml', 'r', encoding='utf-8') as f:
-        cont = f.read()
-        config = yaml.load(cont, Loader=yaml.BaseLoader)
-        with open('config.yml', 'w', encoding='utf-8') as f:
-            config['token'] = '0'
-            yaml.dump(config, f, default_flow_style=False, encoding='utf-8', allow_unicode=True)
 
 
 def get_pic_from_clipboard(window):
@@ -125,9 +120,9 @@ class ThreadKey(Thread):
             status = get_pic_from_clipboard(self.window)
             # toaster = ToastNotifier()
             if status:
-                wx.CallAfter(self.window.Uploading)
+                CallAfter(self.window.Uploading)
             else:
-                wx.CallAfter(self.window.UploadFailed)
+                CallAfter(self.window.UploadFailed)
 
     @staticmethod
     def is_pressed():
@@ -152,61 +147,56 @@ class ThreadUpload(Thread):
         try:
             url = Smms.upload(self.filename, token=self.token)
             copy(url)
-            wx.CallAfter(self.window.UploadSuccess)
+            CallAfter(self.window.UploadSuccess)
         except ConnectionRefusedError:
-            wx.CallAfter(self.window.LoginAgain)
+            CallAfter(self.window.LoginAgain)
         except ConnectionAbortedError:
-            wx.CallAfter(self.window.UploadMax)
+            CallAfter(self.window.UploadMax)
 
 
-# class ThreadUpload(Thread):
-#     def __init__(self, , window):
-#         Thread.__init__(self)
-#         self.window = window
-#
-#     def run(self):
-
-class Trans(wx.Frame):
+class Trans(Frame):
     def __init__(self, parent, title=' ', size=(700, 500)):
-        wx.Frame.__init__(self, parent, title=title, size=size, style=wx.STAY_ON_TOP)
+        Frame.__init__(self, parent, title=title, size=size, style=STAY_ON_TOP)
 
-        label_user = wx.StaticText(self, -1, title, pos=(0, 0))
+        label_user = StaticText(self, -1, title, pos=(0, 0))
 
         # self.Text = wx.TextCtrl(self, size=(700, 500))
         # self.Text.SetBackgroundColour('Black'), self.Text.SetForegroundColour('Steel Blue')
         self.SetTransparent(100)  # 设置透明
 
 
-class MyTaskBarIcon(wx.adv.TaskBarIcon):
+class MyTaskBarIcon(TaskBarIcon):
     ICON = "logo.ico"  # 图标地址
-    ID_ABOUT = wx.NewIdRef()  # 菜单选项“关于”的ID
-    ID_EXIT = wx.NewIdRef()  # 菜单选项“退出”的ID
-    ID_UPLOAD = wx.NewIdRef()  # 菜单选项“显示页面”的ID
-    ID_LOGIN = wx.NewIdRef()  # 菜单选项“显示页面”的ID
+    ID_ABOUT = NewIdRef()  # 菜单选项“关于”的ID
+    ID_EXIT = NewIdRef()  # 菜单选项“退出”的ID
+    ID_UPLOAD = NewIdRef()  # 菜单选项“显示页面”的ID
+    ID_LOGIN = NewIdRef()  # 菜单选项“显示页面”的ID
     TITLE = "SMPIC"  # 鼠标移动到图标上显示的文字
 
     def __init__(self):
-        wx.adv.TaskBarIcon.__init__(self)
-        self.SetIcon(wx.Icon(self.ICON), self.TITLE)  # 设置图标和标题
-        self.Bind(wx.EVT_MENU, self.onAbout, id=self.ID_ABOUT)  # 绑定“关于”选项的点击事件
-        self.Bind(wx.EVT_MENU, self.onExit, id=self.ID_EXIT)  # 绑定“退出”选项的点击事件
-        self.Bind(wx.EVT_MENU, self.onUpload, id=self.ID_UPLOAD)
-        self.Bind(wx.EVT_MENU, self.OnLogin, id=self.ID_LOGIN)
+        TaskBarIcon.__init__(self)
+        self.SetIcon(Icon(self.ICON), self.TITLE)  # 设置图标和标题
+        self.Bind(EVT_MENU, self.onAbout, id=self.ID_ABOUT)  # 绑定“关于”选项的点击事件
+        self.Bind(EVT_MENU, self.onExit, id=self.ID_EXIT)  # 绑定“退出”选项的点击事件
+        self.Bind(EVT_MENU, self.onUpload, id=self.ID_UPLOAD)
+        self.Bind(EVT_MENU, self.OnLogin, id=self.ID_LOGIN)
 
-        self.frame = wx.Frame(parent=None, title='登录', size=(250, 160))
+        self.frame = Frame(parent=None, title='登录', size=(285, 160))
 
-        panel = wx.Panel(self.frame, -1)
-        label_user = wx.StaticText(panel, -1, "账号:", pos=(10, 10))
-        label_pass = wx.StaticText(panel, -1, "密码:", pos=(10, 50))
+        panel = Panel(self.frame, -1)
+        label_user = StaticText(panel, -1, "账号:", pos=(10, 10))
+        label_pass = StaticText(panel, -1, "密码:", pos=(10, 50))
 
-        self.entry_user = wx.TextCtrl(panel, -1, size=(160, 20), pos=(50, 10))
-        self.entry_pass = wx.TextCtrl(panel, -1, size=(160, 20), pos=(50, 50), style=wx.TE_PASSWORD)
+        self.entry_user = TextCtrl(panel, -1, size=(210, 20), pos=(50, 10))
+        self.entry_pass = TextCtrl(panel, -1, size=(210, 20), pos=(50, 50), style=TE_PASSWORD)
 
-        self.but_login = wx.Button(panel, -1, "登陆", size=(50, 30), pos=(10, 80))
-        self.but_register = wx.Button(panel, -1, "注册SM.MS账号", size=(110, 30), pos=(110, 80))
+        self.but_login = Button(panel, -1, "登陆", size=(50, 30), pos=(10, 80))
+        self.but_register = Button(panel, -1, "注册SM.MS账号", size=(110, 30), pos=(150, 80))
+        self.but_not_login = Button(panel, -1, "免登陆使用", size=(80, 30), pos=(65, 80))
 
-        self.but_login.Bind(wx.EVT_BUTTON, self.on_but_login)
-        self.but_register.Bind(wx.EVT_BUTTON, self.on_but_register)
+        self.but_login.Bind(EVT_BUTTON, self.on_but_login)
+        self.but_register.Bind(EVT_BUTTON, self.on_but_register)
+        self.but_not_login.Bind(EVT_BUTTON, self.on_but_not_login)
 
         self.frame.Center()
         token = init_config()
@@ -215,8 +205,8 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
         else:
             self.frame.Show(False)
 
-        self.frame.SetMaxSize((250, 160))
-        self.frame.SetMinSize((250, 160))
+        self.frame.SetMaxSize((285, 160))
+        self.frame.SetMinSize((285, 160))
         ThreadKey(self)
 
         self.frame2 = Trans(parent=None, title='上传中', size=(50, 20))
@@ -237,13 +227,13 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
 
     # “关于”选项的事件处理器
     def onAbout(self, event):
-        wx.MessageBox('Author：Jack Wang\nEmail Address：544907049@qq.com\nLatest Update：2019-12-13', "info")
+        MessageBox('Author：Jack Wang\nEmail Address：544907049@qq.com\nLatest Update：2019-12-13', "info")
 
     # “退出”选项的事件处理器
     def onExit(self, event):
         global listener
         listener.stop()
-        wx.Exit()
+        Exit()
 
     # “显示页面”选项的事件处理器
     def onUpload(self, event):
@@ -254,7 +244,7 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
 
     # 创建菜单选项
     def CreatePopupMenu(self):
-        menu = wx.Menu()
+        menu = Menu()
         for mentAttr in self.getMenuAttrs():
             menu.Append(mentAttr[1], mentAttr[0])
         return menu
@@ -278,15 +268,39 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
                     config['token'] = token
                     yaml.dump(config, f, default_flow_style=False, encoding='utf-8', allow_unicode=True)
             self.frame.Show(False)
-            wx.MessageBox('登陆成功', "温馨提示")
+            MessageBox('登陆成功！\n'
+                '使用方法：\n1.首先复制一张图片，本地的或者网\n'
+                '页上的，或者使用截图工具截图；\n'
+                '2.随后按' + hotkeys + '上传；\n'
+                                    '3.成功后会返回url至你的剪贴板。\n\n提示：\n可编辑config.yml修改快捷键',
+                "温馨提示")
         except KeyError:
-            wx.MessageBox('密码或账号错误', "警告")
+            MessageBox('密码或账号错误', "警告")
+
+    def on_but_not_login(self, event):
+        try:
+            with open('config.yml', 'r', encoding='utf-8') as f:
+                cont = f.read()
+                config = yaml.load(cont, Loader=yaml.BaseLoader)
+                with open('config.yml', 'w', encoding='utf-8') as f:
+                    config['token'] = 'not login'
+                    yaml.dump(config, f, default_flow_style=False, encoding='utf-8', allow_unicode=True)
+            self.frame.Show(False)
+
+            MessageBox(
+                '使用方法：\n1.首先复制一张图片，本地的或者网\n'
+                '页上的，或者使用截图工具截图；\n'
+                '2.随后按' + hotkeys + '上传；\n'
+                '3.成功后会返回url至你的剪贴板。\n\n提示：\n1.免登录使用将无法在线查看你的全部上\n传记录，仅可查看本IP的上传记录\n2.可编辑config.yml修改快捷键',
+                "温馨提示")
+        except KeyError:
+            MessageBox('出现了未知问题，请提交issue', "警告")
 
     def on_but_register(self, event):
         openweb('https://sm.ms/register')
 
     def LoginAgain(self):  # 删除线程
-        wx.MessageBox('登录已失效，需重新登录', "警告")
+        MessageBox('登录已失效，需重新登录', "警告")
         self.frame.Show(True)
 
     def Uploading(self):
@@ -326,16 +340,16 @@ class MyTaskBarIcon(wx.adv.TaskBarIcon):
         timer.start()
 
 
-class MyApp(wx.App):
+class MyApp(App):
     def __init__(self):
-        wx.App.__init__(self)
+        App.__init__(self)
 
         MyFrame()
 
 
-class MyFrame(wx.Frame):
+class MyFrame(Frame):
     def __init__(self):
-        wx.Frame.__init__(self)
+        Frame.__init__(self)
         MyTaskBarIcon()  # 显示系统托盘图标
 
 
